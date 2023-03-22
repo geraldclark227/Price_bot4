7126eadcd443a1a9b383e767880084f2d2f881ed
Q8Shinobi <98072037+Q8Shinobi@users.noreply.github.com>
2023-03-22 01:48:51 -0700

    Update tkinter_final.py

--- tkinter_final.py
+++ tkinter_final.py
@@ -1,164 +1,193 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
 import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+import webbrowser
 
-def run_web_driver():
-    from selenium import webdriver as wd
-    from selenium.webdriver.common.by import By
-    
-    wd = wd.Chrome()
-    wd.get("http://www.amazon.com")
-    wd.implicitly_wait(10)
 
-    #__zip code for locality of order__
-    #wd.find_element(By.ID,"nav-global-location-popover-link").click()
-    #postcode_field = wd.find_element(By.ID,"GLUXZipUpdateInput")
-    #postcode_field.send_keys("91423")
-    #wd.find_element(By.ID,"GLUXZipUpdate").click() 
-    #close_element = wd.find_element(By.CSS_SELECTOR, "button.a-button-text")
-    #close_element.click()
-    #wd.implicitly_wait(10)
-        
-    #search_field = wd.find_element(By.ID, "twotabsearchtextbox").send_keys(my_search)
-    search_field = wd.find_element(By.ID, "twotabsearchtextbox")
-    search_field.send_keys(my_search)
-    search_button_click = wd.find_element(By.ID, "nav-search-submit-button")
+def search_cards():
+    year = year_entry.get()
+    card_set = card_set_entry.get()
+    character = character_entry.get()
+
+    driver = wd.Chrome(executable_path="C:\\Users\\q8mlg\\OneDrive\\Desktop\\driver\\chromedriver.exe")
+    driver.implicitly_wait(10)
+
+    df_amazon = search_amazon(driver, year, card_set, character)
+    df_ebay = search_ebay(driver, year, card_set, character)
+
+    driver.quit()
+
+    df_combined = pd.concat([df_amazon, df_ebay], ignore_index=True)
+
+    display_results(df_combined)
+
+
+def search_amazon(driver, year, card_set, character):
+    driver.get("http://www.amazon.com")
+    my_search = f"{year} {card_set} {character}"
+
+    search_field = driver.find_element(By.ID, "twotabsearchtextbox").send_keys(my_search)
+    search_button_click = driver.find_element(By.ID, "nav-search-submit-button")
     search_button_click.click()
-    wd.implicitly_wait(10)
+    driver.implicitly_wait(10)
 
-    soup = BeautifulSoup(wd.page_source, 'html.parser')
-    web_page = soup.findAll("div",{"data-component-type": "s-search-result"})
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    web_page = soup.findAll("div", {"data-component-type": "s-search-result"})
 
-    row=""
-    result_list_1=[]
+    row = ""
+    result_list_1 = []
 
     for result in web_page:
         title = result.find("span", {"class": "a-size-base-plus a-color-base a-text-normal"})
-        price = result.find("span", {"class": "a-offscreen"}) 
+        price = result.find("span", {"class": "a-offscreen"})
         url = result.find("a", {"class", "a-link-normal s-no-outline"})
         if title and price and url:
-            row = [title.text, 
-                price.text.replace("$","").replace(",",""), 
-                "https://amazon.com/" + url['href']]            
+            row = [title.text,
+                   price.text.replace("$", "").replace(",", ""),
+                   "https://amazon.com/" + url['href']]
         result_list_1.append(row)
+
     # Dataframe Amazon
     df_a = pd.DataFrame.from_records(result_list_1, columns=["Title", "Price", "URL"])
     df_a["Title"] = df_a["Title"].str.strip()
-    #df_a["Price"] = df_a["Price"].astype(float)
-    df_a_f1 = df_a[df_a["Title"].str.match(year1) 
-        & df_a["Title"].str.contains(card_set1, case=False) 
-        & df_a["Title"].str.contains(character1, case=False)]
+    df_a_f1 = df_a[df_a["Title"].str.match(year)
+                   & df_a["Title"].str.contains(card_set, case=False)
+                   & df_a["Title"].str.contains(character, case=False)]
     df_a_f2 = df_a_f1.drop_duplicates(keep="first")
-    #df_a_f3 = df_a_f2.sort_values(by="Price", ascending=False)
-    print(df_a_f2)
 
-    # get ebay.com from webdriver
-    wd.get("https://www.ebay.com/")
-    wd.implicitly_wait(10)
+    # Save Amazon search results to CSV file
+    df_a_f2.to_csv("amazon_cards.csv", index=False)
 
-    search_field_ebay = wd.find_element(By.ID, "gh-ac").send_keys(my_search)
-    search_button_ebay_click = wd.find_element(By.ID, "gh-btn")
+    return df_a_f2
+
+def search_ebay(driver, year, card_set, character):
+    driver.get("https://www.ebay.com/")
+    my_search = f"{year} {card_set} {character}"
+
+    search_field_ebay = driver.find_element(By.ID, "gh-ac").send_keys(my_search)
+    search_button_ebay_click = driver.find_element(By.ID, "gh-btn")
     search_button_ebay_click.click()
-    wd.implicitly_wait(10)
+    driver.implicitly_wait(10)
 
-    soup_ebay = BeautifulSoup(wd.page_source, 'html.parser')
-    web_page_ebay = soup_ebay.findAll("li",{"class": "s-item s-item__pl-on-bottom"})
+    soup_ebay = BeautifulSoup(driver.page_source, 'html.parser')
+    web_page_ebay = soup_ebay.findAll("li", {"class": "s-item s-item__pl-on-bottom"})
 
-    row_ebay=""
-    result_list_2=[]
+    row_ebay = ""
+    result_list_2 = []
 
     for result2 in web_page_ebay:
-        title_ebay = result2.find("span",{"role": "heading"})
-        price_ebay = result2.find("span",{"class": "s-item__price"})
-        #seller_rating = result2.find("span", {"class": "s-item__seller-info-text"})
+        title_ebay = result2.find("span", {"role": "heading"})
+       
+        price_ebay = result2.find("span", {"class": "s-item__price"})
         url_ebay = result2.find("a", {"class": "s-item__link"})
         if title_ebay and price_ebay and url_ebay:
-            row_ebay = [title_ebay.text.replace("🔥", "").replace("💎","").replace("'",""),
-                        price_ebay.text.replace("$","").replace(",",""),
+            row_ebay = [title_ebay.text,
+                        price_ebay.text.replace("$", "").replace(",", ""),
                         url_ebay['href']]
         result_list_2.append(row_ebay)
 
-    # Dataframe Ebay
-    df_eb = pd.DataFrame.from_records(result_list_2, columns=["Title", "Price", "URL"])
-    df_eb["Title"] = df_eb["Title"].str.strip()
-    df_eb2 = df_eb[~df_eb["Price"].str.contains("to", case=False)]
-    #df_eb2["Price"] = df_eb2["Price"].astype(float)
-    df_eb_f1 = df_eb2[df_eb2["Title"].str.contains(year1) 
-        & df_eb2["Title"].str.contains(card_set1, case=False) 
-        & df_eb2["Title"].str.contains(character1, case=False)]
-    df_eb_f2 = df_eb_f1.drop_duplicates(keep="first")
-    #df_eb_f3 = df_eb_f2.sort_values(by="Price", ascending=False)
-    print(df_eb_f2)
+    # Dataframe eBay
+    df_e = pd.DataFrame.from_records(result_list_2, columns=["Title", "Price", "URL"])
+    df_e["Title"] = df_e["Title"].str.strip()
+    df_e_f1 = df_e[df_e["Title"].str.match(year)
+                   & df_e["Title"].str.contains(card_set, case=False)
+                   & df_e["Title"].str.contains(character, case=False)]
+    df_e_f2 = df_e_f1.drop_duplicates(keep="first")
 
-    #webdriver close
-    #wd.quit()
-    wd.close()
+    # Save eBay search results to CSV file
+    df_e_f2.to_csv("ebay_cards.csv", index=False)
 
-    df_results_all = pd.concat([df_a_f2, df_eb_f2], axis=0)
-    df_results_all["Price"] = df_results_all["Price"].astype(float)
-    df_results_all1 = df_results_all.sort_values(by="Price", ascending=False)
-    df_results_all1.to_csv("marvel_cards.csv", index=False)
-    df_results_all1.to_excel("marvel_cards.xlsx")
-    print(df_results_all1)
+    return df_e_f2
+
+def display_results(df):
+    results_window = tk.Toplevel(root)
+    results_window.title("Search Results")
+
+    tree = ttk.Treeview(results_window, columns=("Title", "Price", "URL"), show="headings")
+    tree.column("Title", minwidth=0, width=300)
+    tree.column("Price", minwidth=0, width=100)
+    tree.column("URL", minwidth=0, width=300)
+
+    tree.heading("Title", text="Title")
+    tree.heading("Price", text="Price")
+    tree.heading("URL", text="URL")
+
+    for index, row in df.iterrows():
+        tree.insert("", "end", values=(row["Title"], row["Price"], row["URL"]))
+
+    tree.pack()
+
+    save_button = ttk.Button(results_window, text="Save Results", command=lambda: save_results(df))
+    save_button.pack()
+import os
+
+def open_url(event, tree):
+    item = tree.item(tree.focus())['values']
+    if item:
+        url = item[2]
+        webbrowser.open(url, new=1)
 
 
-import tkinter as tk
-import customtkinter
-import numpy
-from tkinter import ttk, filedialog
+def display_results(df):
+    results_window = tk.Toplevel(root)
+    results_window.title("Search Results")
 
-#def open_excel():
-    #my_file1 = 
+    tree = ttk.Treeview(results_window, columns=("Title", "Price"), show="headings")
+    tree.column("Title", minwidth=0, width=300)
+    tree.column("Price", minwidth=0, width=100)
+
+    tree.heading("Title", text="Title")
+    tree.heading("Price", text="Price")
+
+    for index, row in df.iterrows():
+        tree.insert("", "end", values=(row["Title"], row["Price"], row["URL"]))
+
+    tree.bind("<Double-1>", lambda event: open_url(event, tree))  # Bind the open_url function to a double-click event
+
+    tree.pack()
+
+    save_button = ttk.Button(results_window, text="Save Results", command=lambda: save_results(df))
+    save_button.pack()
 
 
+
+# GUI code
 root = tk.Tk()
-root.geometry("850x400")
-root.title("Marvel Cards")
-root.pack_propagate(False)
+root.title("Marvel Card Search")
 
-year_var = tk.StringVar()
-card_set_var = tk.StringVar()
-character_var = tk.StringVar()
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 
+year_label = ttk.Label(mainframe, text="Year:")
+year_label.grid(column=1, row=1, sticky=tk.W)
+year_entry = ttk.Entry(mainframe)
+year_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
 
-def submit():
-    global year1
-    year1 = year_var.get()
-    global card_set1
-    card_set1 = card_set_var.get()
-    global character1
-    character1 = character_var.get()
-    global my_search
-    #my_search = str(year1)+" "+str(card_set1)+" "+str(character1)
-    my_search = f"{year1} {card_set1} {character1}"
+card_set_label = ttk.Label(mainframe, text="Card Set:")
+card_set_label.grid(column=1, row=2, sticky=tk.W)
+card_set_entry = ttk.Entry(mainframe)
+card_set_entry.grid(column=2, row=2, sticky=(tk.W, tk.E))
 
-    print("year: " + year1)
-    print("card set: " + card_set1)
-    print("character: " + character1)
-    
-    year_var.set("")
-    card_set_var.set("")
-    character_var.set("")
+character_label = ttk.Label(mainframe, text="Character:")
+character_label.grid(column=1, row=3, sticky=tk.W)
+character_entry = ttk.Entry(mainframe)
+character_entry.grid(column=2, row=3, sticky=(tk.W, tk.E))
 
-yr_label = tk.Label(root, text="year", font=("Helvetica", 18, "normal"))
-yr_entry = tk.Entry(root, textvariable = year_var, font=("Helvetica", 18, "normal"), width = 35)
-#yr_entry.focus_set()
-c_set_label = tk.Label(root, text="card set", font=("Helvetica", 18, "normal"))
-c_set_entry = tk.Entry(root, textvariable = card_set_var, font=("Helvetica", 18, "normal"), width = 35)
+search_button = ttk.Button(mainframe, text="Search", command=search_cards)
+search_button.grid(column=2, row=4, sticky=(tk.W, tk.E))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+mainframe.columnconfigure(0, weight=1)
+mainframe.columnconfigure(1, weight=1)
+mainframe.columnconfigure(2, weight=1)
+mainframe.columnconfigure(3, weight=1)
+mainframe.rowconfigure(0, weight=1)
+mainframe.rowconfigure(1, weight=1)
+mainframe.rowconfigure(2, weight=1)
+mainframe.rowconfigure(3, weight=1)
+mainframe.rowconfigure(4, weight=1)
 
-character_label = tk.Label(root, text="character", font=("Helvetica", 18, "normal"))
-character_entry = tk.Entry(root, textvariable = character_var, font=("Helvetica", 18, "normal"), width = 35)
-
-sub_btn = tk.Button(root, text="Submit", command = submit)
-run_web_driver_btn = tk.Button(root, text="Run Webdriver", command = run_web_driver)
-
-yr_label.grid(row=0, column=0, padx=5, pady=5)
-yr_entry.grid(row=0, column=1, padx=5, pady=5)
-c_set_label.grid(row=1, column=0, padx=5, pady=5)
-c_set_entry.grid(row=1, column=1, padx=5, pady=5)
-character_label.grid(row=3, column=0, padx=5, pady=5)
-character_entry.grid(row=3, column=1, padx=5, pady=5)
-sub_btn.grid(row=4, column=1, padx=5, pady=5)
-run_web_driver_btn.grid(row=5, column=1, padx=5, pady=5)
-
-#tkinter mainloop
 root.mainloop()
